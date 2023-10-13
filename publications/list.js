f00d217a8896a5b03bb5e6ab3bcd224592623664
txdai <txdai16@gmail.com>
2023-10-12 22:17:24 -0700

    Add initial content

---
+++ publications/list.js
@@ -0,0 +1,102 @@
+// publications/list.js
+const publicationsList = [
+
+    {
+        imageSrc: 'publications/images/10.1515_nanoph-2022-0109.png',
+        title: 'Matrix eigenvalue solver based on reconfigurable photonic neural network',
+        authors: 'Kun Liao*, Chentong Li*, <strong>Tianxiang Dai*</strong>, Chuyu Zhong, Hongtao Lin, Xiaoyong Hu and Qihuang Gong',
+        journal: 'Nanophotonics',
+        pdfLink: 'publications/pdfs/10.1515_nanoph-2022-0109.pdf',
+        journalLink: 'https://www.degruyter.com/document/doi/10.1515/nanoph-2022-0109',
+        videoLink: '',
+        codeLink: '',
+        dataLink: '',
+        websiteLink: '',
+        slidesLink: '',
+        bibtexData:
+            `@article{txdai2022nanoph,
+	author = {Kun Liao and Chentong Li and Tianxiang Dai and Chuyu Zhong and Hongtao Lin and Xiaoyong Hu and Qihuang Gong},
+	doi = {doi:10.1515/nanoph-2022-0109},
+	journal = {Nanophotonics, 2022.},
+	lastchecked = {2023-10-13},
+	number = {17},
+	pages = {4089--4099},
+	url = {https://doi.org/10.1515/nanoph-2022-0109},
+	volume = {11},
+	year = {2022},
+	bdsk-url-1 = {https://doi.org/10.1515/nanoph-2022-0109}}
+`,
+        selected: true, // Indicate whether the study is selected
+    },
+
+    {
+        imageSrc: 'publications/images/s41378-023-00588-y.png',
+        title: 'Snapshot Mueller spectropolarimeter imager',
+        authors: '<strong>Tianxiang Dai</strong>, Thaibao Phan, Evan W. Wang, Soonyang Kwon, Jaehyeon Son, Myungjun Lee and Jonathan A. Fan',
+        journal: 'Microsystems & Nanoengineering, 2023.',
+        pdfLink: 'publications/pdfs/s41378-023-00588-y.pdf',
+        journalLink: 'https://doi.org/10.1038/s41378-023-00588-y',
+        videoLink: '',
+        codeLink: '',
+        dataLink: '',
+        websiteLink: '',
+        slidesLink: '',
+        bibtexData:
+            `
+@article{txdai2023micronano,
+	author = {Dai, Tianxiang and Phan, Thaibao and Wang, Evan W. and Kwon, Soonyang and Son, Jaehyeon and Lee, Myungjun and Fan, Jonathan A.},
+	doi = {10.1038/s41378-023-00588-y},
+	isbn = {2055-7434},
+	journal = {Microsystems \& Nanoengineering},
+	number = {1},
+	pages = {125},
+	title = {Snapshot Mueller spectropolarimeter imager},
+	url = {https://doi.org/10.1038/s41378-023-00588-y},
+	volume = {9},
+	year = {2023},
+	bdsk-url-1 = {https://doi.org/10.1038/s41378-023-00588-y}}
+`,
+        selected: true, // Indicate whether the study is selected
+    },
+
+    {
+        imageSrc: 'publications/images/acsphotonics.2c01516.png',
+        title: 'Integrated Photonic Neural Networks: Opportunities and Challenges',
+        authors: 'Kun Liao, <strong>Tianxiang Dai</strong>, Qiuchen Yan, Xiaoyong Hu and Qihuang Gong',
+        journal: 'ACS Photonics, 2023.',
+        pdfLink: 'publications/pdfs/acsphotonics.2c01516.pdf',
+        journalLink: 'https://doi.org/10.1364/OE.388378',
+        videoLink: '',
+        codeLink: '',
+        dataLink: '',
+        websiteLink: '',
+        slidesLink: '',
+        bibtexData:
+            `
+@article{txdai2023acsphotonics,
+	annote = {doi: 10.1021/acsphotonics.2c01516},
+	author = {Liao, Kun and Dai, Tianxiang and Yan, Qiuchen and Hu, Xiaoyong and Gong, Qihuang},
+	date = {2023/07/19},
+	date-added = {2023-10-12 19:26:32 -0700},
+	date-modified = {2023-10-12 19:26:32 -0700},
+	doi = {10.1021/acsphotonics.2c01516},
+	journal = {ACS Photonics},
+	journal1 = {ACS Photonics},
+	month = {07},
+	number = {7},
+	pages = {2001--2010},
+	publisher = {American Chemical Society},
+	title = {Integrated Photonic Neural Networks: Opportunities and Challenges},
+	type = {doi: 10.1021/acsphotonics.2c01516},
+	url = {https://doi.org/10.1021/acsphotonics.2c01516},
+	volume = {10},
+	year = {2023},
+	year1 = {2023},
+	bdsk-url-1 = {https://doi.org/10.1021/acsphotonics.2c01516}}
+}`,
+        selected: true, // Indicate whether the study is selected
+    },
+
+];
+
+export default publicationsList;
