c21386b02bbbbcdea356f90f057dc99f599938ae
txdai <txdai16@gmail.com>
2023-10-31 04:44:32 -0700

    Add publication

--- publications/list.js
+++ publications/list.js
@@ -97,6 +97,41 @@ const publicationsList = [
         selected: true, // Indicate whether the study is selected
     },
 
+    {
+        imageSrc: 'publications/images/s41467-023-42116-6.png',
+        title: 'Graphene/silicon heterojunction for reconfigurable phase-relevant activation function in coherent optical neural networks',
+        authors: 'Chuyu Zhong, Kun Liao, <strong>Tianxiang Dai</strong>, Maoliang Wei, Hui Ma, Jianghong Wu, Zhibin Zhang, Yuting Ye, Ye Luo, Zequn Chen, Jialing Jian, Chunlei Sun, Bo Tang, Peng Zhang, Ruonan Liu, Junying Li, Jianyi Yang, Lan Li, Kaihui Liu, Xiaoyong Hu and Lin, Hongtao',
+        journal: 'Nature Communications, 2023.',
+        pdfLink: 'publications/pdfs/s41467-023-42116-6.pdf',
+        journalLink: 'https://doi.org/10.1038/s41467-023-42116-6',
+        videoLink: '',
+        codeLink: '',
+        dataLink: '',
+        websiteLink: '',
+        slidesLink: '',
+        bibtexData:
+            `
+@article{txdai2023naturecomm,
+	abstract = {Optical neural networks (ONNs) herald a new era in information and communication technologies and have implemented various intelligent applications. In an ONN, the activation function (AF) is a crucial component determining the network performances and on-chip AF devices are still in development. Here, we first demonstrate on-chip reconfigurable AF devices with phase activation fulfilled by dual-functional graphene/silicon (Gra/Si) heterojunctions. With optical modulation and detection in one device, time delays are shorter, energy consumption is lower, reconfigurability is higher and the device footprint is smaller than other on-chip AF strategies. The experimental modulation voltage (power) of our Gra/Si heterojunction achieves as low as 1 V (0.5 mW), superior to many pure silicon counterparts. In the photodetection aspect, a high responsivity of over 200 mA/W is realized. Special nonlinear functions generated are fed into a complex-valued ONN to challenge handwritten letters and image recognition tasks, showing improved accuracy and potential of high-efficient, all-component-integration on-chip ONN. Our results offer new insights for on-chip ONN devices and pave the way to high-performance integrated optoelectronic computing circuits.},
+	author = {Zhong, Chuyu and Liao, Kun and Dai, Tianxiang and Wei, Maoliang and Ma, Hui and Wu, Jianghong and Zhang, Zhibin and Ye, Yuting and Luo, Ye and Chen, Zequn and Jian, Jialing and Sun, Chunlei and Tang, Bo and Zhang, Peng and Liu, Ruonan and Li, Junying and Yang, Jianyi and Li, Lan and Liu, Kaihui and Hu, Xiaoyong and Lin, Hongtao},
+	date = {2023/10/31},
+	date-added = {2023-10-31 04:37:41 -0700},
+	date-modified = {2023-10-31 04:37:41 -0700},
+	doi = {10.1038/s41467-023-42116-6},
+	id = {Zhong2023},
+	isbn = {2041-1723},
+	journal = {Nature Communications},
+	number = {1},
+	pages = {6939},
+	title = {Graphene/silicon heterojunction for reconfigurable phase-relevant activation function in coherent optical neural networks},
+	url = {https://doi.org/10.1038/s41467-023-42116-6},
+	volume = {14},
+	year = {2023},
+	bdsk-url-1 = {https://doi.org/10.1038/s41467-023-42116-6}}
+`,
+        selected: true, // Indicate whether the study is selected
+    },
+
 ];
 
 export default publicationsList;
